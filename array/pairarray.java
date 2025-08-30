e994757a8361ed2a839db52aa3b42ff08af1799e
Anshuman Singh <singhanshuman329@gmail.com>
2025-08-30 08:20:49 +0530

    Pairs in array

---
+++ array/pairarray.java
@@ -0,0 +1,18 @@
+class pair{
+public static void pairs(int numbers[]){
+int tp =0;
+for (int i=0; i< numbers.length; i++){
+int curr = numbers[i];
+for (int j =i+1; j<numbers.length; j++){
+System.out.print("{"+ curr + ","+ numbers[j] + "}");
+tp++;
+}
+System.out.println();
+}
+System.out.println("Total no of pairs = "+ tp);
+}
+public static void main(String[]args){
+int numbers[]={2,4,6,8,9,12};
+pairs(numbers);
+}
+}
