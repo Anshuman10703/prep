b7f8f38429692596b9960faa5aa87335bdba5012
Anshuman Singh <singhanshuman329@gmail.com>
2025-09-27 19:07:21 +0530

    Fibonnaci

---
+++ Maths/fibonnaci.java
@@ -0,0 +1,17 @@
+class Fibonnaci{
+public static int nthFibonnaci(int n){
+ if (n==0) return 0;
+ if(n==1) return 1;
+int a=0 , b=1, c=0; 
+for(int i = 2; i<=n; i++){
+ c=a+b ;
+ a=b;
+ b=c;
+}
+return b;
+}
+public static void main(String [] args){
+int n =5 ; 
+System.out.println(nthFibonnaci(n));
+}
+}
